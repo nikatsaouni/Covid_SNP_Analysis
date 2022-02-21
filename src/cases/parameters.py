150bc1765b42424c069b00fe418033e5a039c990
katsaouni <nikatsaouni@gmail.com>
2022-02-21 20:30:44 +0100

    change permissions

--- src/cases/parameters.py
+++ src/cases/parameters.py
@@ -1,7 +1,11 @@
+import src.d00_utils.project_path as path
+
+
+snp_folder = path.proj_dir + "/data/01_raw/Corrected_csv_files_folder/"
 
 # Directories with data
-snp_folder = "/Users/nikatsaouni/Documents/Disease_Prediction/Covid_Proj/CovidSNPAnalysis/data/01_raw/Corrected_csv_files_folder/"
-patient_data =  "/Users/nikatsaouni/Documents/Disease_Prediction/Covid_Proj/CovidSNPAnalysis/data/01_raw/Tabelle_Endfassung_040521.xlsx"
+# snp_folder = "/Users/nikatsaouni/Documents/Disease_Prediction/Covid_Proj/Covid_SNP_Analysis/data/01_raw/Corrected_csv_files_folder/"
+patient_data = path.proj_dir + "data/01_raw/Tabelle_Endfassung_040521.xlsx"
 
 # Parameters for calculations
 cutoff_SNPs = 4  # set the minimum number of SNPs per clinical score to be accounted for the calculations
@@ -9,9 +13,9 @@ clinic_score_split = 3  # How to split the data to high and low clinical score
 senario = "Case_2"
 
 # Directories to save results
-Interm_Results = "/Users/nikatsaouni/Documents/Disease_Prediction/Covid_Proj/CovidSNPAnalysis/data/02_intermediate/" + senario + "/"
-Processed_Results = "/Users/nikatsaouni/Documents/Disease_Prediction/Covid_Proj/CovidSNPAnalysis/data/03_processed/" + senario + "/"
-Model_Results = "/Users/nikatsaouni/Documents/Disease_Prediction/Covid_Proj/CovidSNPAnalysis/data/05_model_output/" + senario + "/"
-Report_Results = "/Users/nikatsaouni/Documents/Disease_Prediction/Covid_Proj/CovidSNPAnalysis/data/06_reporting/" + senario + "/"
+Interm_Results = path.proj_dir + "data/02_intermediate/" + senario + "/"
+Processed_Results = path.proj_dir + "data/03_processed/" + senario + "/"
+Model_Results = path.proj_dir + "data/05_model_output/" + senario + "/"
+Report_Results = path.proj_dir + "data/06_reporting/" + senario + "/"
 
 
