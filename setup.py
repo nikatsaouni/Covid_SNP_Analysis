2e4286fb64e7961970f53cdd578ad738a2f16577
katsaouni <nikatsaouni@gmail.com>
2022-02-21 19:48:11 +0100

    initial comiit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='CovidSNPAnalysis',
+    version='',
+    packages=['src', 'src.cases', 'src.d00_utils', 'src.d03_modelling', 'src.d02_processing',
+              'src.d01_data_intermediate', 'tests'],
+    url='',
+    license='',
+    author='nikatsaouni',
+    author_email='katsaouni@em.uni-frankfurt.com',
+    description=''
+)
